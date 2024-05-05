071df3179490feffa8d07fc9f4c4054916ce2163
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2024-05-05 16:52:49 +0300

    add cerror

--- language/src/main/resources/islispprelude.lisp
+++ language/src/main/resources/islispprelude.lisp
@@ -74,6 +74,15 @@
                 'format-arguments obj)
         nil))
 
+(defun cerror (continue-string error-string :rest obj)
+    (signal-condition
+        (create (class <simple-error>)
+                'format-string error-string
+                'format-arguments obj)
+        (let ((str (create-string-output-stream)))
+          (funcall #'format str continue-string obj)
+          (get-output-stream-string str))))
+
 (defclass <program-error> (<error>) ())
 
 (defclass <conversion-error> (<error>)
